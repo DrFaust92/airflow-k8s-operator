1683e4defb6d422429ae80bc850cf2beba605d79
drfaust92 <ilia.lazebnik@gmail.com>
2025-11-23 22:03:17 -0500

    add liveness probe to helm chart

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import variables  # noqa: F401
 import connections  # noqa: F401
+import datetime
+import kopf
+
+@kopf.on.probe(id='now')
+def get_current_timestamp(**kwargs):
+    return datetime.datetime.now(datetime.timezone.utc).isoformat()
