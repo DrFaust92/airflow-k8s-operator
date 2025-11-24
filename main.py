b489336242e85cf5240f1dc657be7bc8ee14ba76
drfaust92 <ilia.lazebnik@gmail.com>
2025-11-23 22:42:06 -0500

    ruff format

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import resources.connections  # noqa: F401
 import datetime
 import kopf
 
-@kopf.on.probe(id='now')
+
+@kopf.on.probe(id="now")
 def get_current_timestamp(**kwargs):
     return datetime.datetime.now(datetime.timezone.utc).isoformat()
